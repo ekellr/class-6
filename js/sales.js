3e4105d48c30002366293c1dfc0d42d7e705cff2
ekellr <ekellr@hotmail.com>
2017-06-14 08:44:57 -0700

    defined initial data and loaded first one on page load

--- js/sales.js
+++ js/sales.js
@@ -2,6 +2,39 @@
 
 'use strict';
 
+var storeData = [];
+
+var bellevue = new Object();
+bellevue.Name = "Bellevue";
+bellevue.MinCustomersPerHour = 10;
+bellevue.MaxCustomersPerHour = 200;
+bellevue.AvgCookiesPerCustomer = 2;
+storeData.push(bellevue);
+
+var redmond = new Object();
+redmond.Name = "Redmond";
+redmond.MinCustomersPerHour = 53;
+redmond.MaxCustomersPerHour = 500;
+redmond.AvgCookiesPerCustomer = 4;
+storeData.push(redmond);
+
+
+var changeStore = function(storeIndex)
+{
+  var store = storeData[storeIndex];
+  document.getElementById('storeName').value = store.Name;
+  document.getElementById('minCookiesPerCustomer').value = store.MinCustomersPerHour;
+  document.getElementById('maxCookiesPerCustomer').value = store.MaxCustomersPerHour;
+  document.getElementById('avgCookiesPerCustomer').value = store.AvgCookiesPerCustomer;
+}
+
+var saveData = function() {
+    alert('saving data!');
+}
+
+
+var currentIndex = 0;
+
 
 
 var img = document.createElement('img');
