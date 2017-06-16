134a7694e8c35a03ad262a8f208ababfb35b34fb
ekellr <ekellr@hotmail.com>
2017-06-15 22:21:16 -0700

    Added pictures to the project folder

--- js/index.js
+++ js/index.js
@@ -7,3 +7,8 @@ var img = document.createElement('img');
 img.setAttribute('src', 'salmon.png');
 img.setAttribute('alt', 'salmon ' );
 document.body.appendChild(img);
+
+var img2 = document.createElement('img2');
+img.setAttribute('src', 'shirt.jpg');
+img.setAttribute('alt', 'shirt ' );
+document.body.appendChild(img);
